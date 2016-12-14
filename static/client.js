d07a3c0e7d54464bcdf1ed30669e9085eff8d2fd
Leif Coleman <mageleif@yahoo.com>
2016-12-14 10:42:08 -0600

    Add execution time test

--- static/client.js
+++ static/client.js
@@ -1,12 +1,17 @@
 var socket = io();
 
+var start;
+
 $("form").submit(function(){
+  start = new Date();
   socket.emit("message_c2s", $("#m").val());
   $("#m").val("");
   return false;
 });
 
 socket.on("message_s2c", function(msg){
+  var end = new Date() - start;
+  console.info("Execution time: %dms", end);
   $("#messages").append($("<li>").text(msg));
 });
 
